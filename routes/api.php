25b213ad4d7cf27a0fd10a3fc784e90d615ef733
Curtis Delicata <curtis@liberugroup.com>
2026-08-29 18:19:21 +0100

    Synchronize maintenance module (#1)
    Co-authored-by: curtisdelicata <bot@liberu.software>

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Liberu\Modules\Maintenance\PreventativeMaintenance\Api\Http\Controllers\MaintenancePlanController;
+
+Route::middleware('auth:sanctum')->prefix('api/v1/maintenance/preventative-maintenance')->group(function (): void {
+    Route::get('/', [MaintenancePlanController::class, 'index']);
+    Route::post('/', [MaintenancePlanController::class, 'store']);
+    Route::get('/{maintenancePlan}', [MaintenancePlanController::class, 'show']);
+    Route::patch('/{maintenancePlan}', [MaintenancePlanController::class, 'update']);
+    Route::delete('/{maintenancePlan}', [MaintenancePlanController::class, 'destroy']);
+});
